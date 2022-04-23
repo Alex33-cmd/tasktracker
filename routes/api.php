697002f595a176d287924d03b9a7ed58aad506a8
Alex33-cmd <alaster586@gmail.com>
2022-04-23 21:33:49 +0300

    API routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ItemController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/items', [ItemController::class, 'index']);
+Route::prefix('/item')->group(function() {
+    Router::post('/store', [ItemController::class, 'store']);
+    Router::put('/id', [ItemController::class, 'update']);
+    Router::delete('/id', [ItemController::class, 'destroy']);
+    }
+);
