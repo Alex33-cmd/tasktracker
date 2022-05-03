749903f20da1f3357d2c2efde7e2ae0bc08234cd
Alex33-cmd <62179831+Alex33-cmd@users.noreply.github.com>
2022-05-03 21:53:10 +0300

    minor style tweaks

--- resources/js/components/AppComponent.vue
+++ resources/js/components/AppComponent.vue
@@ -12,7 +12,7 @@
                 <option value="user_name">Исполнитель</option>
                 <option value="task_status">Статус</option>
                 <option value="task_completion_date">Дата завершения</option>
-                <option value="completed_at">Завершена</option>
+                <option value="completed_at">Дата выполнения</option>
                 <option value="created_at">Дата создания</option>
             </select>
             <list-view 
@@ -72,15 +72,17 @@ import listView from "./ListView.vue"
 
 <style lang="scss" scoped>
 .todoListContainer {
+    background: #000;
     display: grid;
     /* grid-template-columns: repeat(2, 1fr); */
     grid-template-columns: 1fr 2fr;
-    gap: 20px;
+    /* gap: 10px; */
     width: 100%;
-    margin: auto;
+    /* margin: auto; */
     .heading {
         background: #e6e6e6;
         padding: 20px;
+        margin: 20px;
         .title {
             text-align: center;
         }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
+        <style> body { margin: 0 !important; } </style>
     </head>
     <body>
         <div id="app">
